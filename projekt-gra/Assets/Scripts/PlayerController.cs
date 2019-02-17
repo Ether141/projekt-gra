2023455ec0946391204908f2d498dcde442f4d02
Ether141 <majster37@vp.pl>
2019-02-17 17:37:59 +0100

    Movement

---
+++ projekt-gra/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        rb.velocity = movement;
+    }
+}
