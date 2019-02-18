cd6abb1cb2634e952bdcc4f1f80be345d72add15
xQoNam <kandeckigs1234@interia.pl>
2019-02-18 16:51:56 +0100

    Changed .GetComponent to gameObject.transform

--- projekt-gra/Assets/Scripts/EnemyMovement.cs
+++ projekt-gra/Assets/Scripts/EnemyMovement.cs
@@ -11,7 +11,7 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        playerPosition = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
     }
 
     // Update is called once per frame
