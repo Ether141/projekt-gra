17f9ddc037b949ead0b53b726f35826f727c472d
Łukasz <ldrzensla@yahoo.com>
2019-02-21 21:47:45 +0100

    Fixing temp map

--- projekt-gra/Assets/Scripts/Player/PlayerController.cs
+++ projekt-gra/Assets/Scripts/Player/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Movement settings")]
     [Tooltip("Speed of normal walk")] public float speed = 80;
-    [Tooltip("Speed of sprint")] public float sprintSpeed = 110;
+    [Tooltip("Speed of normal walk")] public float sprintSpeed = 110;
     [Tooltip("Can player move")] public bool canMove = true;
     [Tooltip("Can player sprint")] public bool canSprint = true;
 
@@ -15,25 +15,24 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Is player sprinting")] public bool isSprinting = false;
     [Tooltip("Is player moving")] public bool isMoving = false;
 
-    [Header("Other")]
-    [SerializeField] private PlayerStats stats;
-
     private Rigidbody2D rb;
 
     private float moveHorizontal = 0;
     private float moveVertical = 0;
-    private Vector2 movement = Vector2.zero;
-    private float lastRot = 0;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        SetRotation();
+    }
+
     void LateUpdate()
     {
         Movement();
-        SetRotation();                  //I think, we won't need that function in future, so it'll be removed in future
     }
 
     private void Movement ()
@@ -42,27 +41,29 @@ public class PlayerController : MonoBehaviour
         moveVertical = Input.GetAxis("Vertical");
 
         isMoving = (moveHorizontal != 0 || moveVertical != 0) && canMove;
-        isSprinting = canSprint && Input.GetButton("Sprint") && canMove && isMoving && (stats.actualStamina > 0);
+        isSprinting = canSprint && Input.GetButton("Sprint") && canMove;
 
-        movement = (canMove ? new Vector2(moveHorizontal, moveVertical) : Vector2.zero);
-        movement.Normalize();
+        Vector2 movement = (canMove ? new Vector2(moveHorizontal, moveVertical) : Vector2.zero);
         rb.velocity = movement * (isSprinting ? sprintSpeed : speed) * Time.deltaTime;
-
-        if(isSprinting)
-            stats.MinusStamina(1);
     }
 
     private void SetRotation ()
     {
-        if (isMoving)
-        {
-            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
-            lastRot = angle;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-        else
-        {
-            transform.rotation = Quaternion.AngleAxis(lastRot, Vector3.forward);
-        }
+        if (moveHorizontal == 0 && moveVertical > 0)                    //Going up
+            transform.localEulerAngles = new Vector3(0, 0, 90);         
+        else if (moveHorizontal == 0 && moveVertical < 0)               //Going down
+            transform.localEulerAngles = new Vector3(0, 0, -90);        
+        else if(moveHorizontal > 0 && moveVertical == 0)                //Going right
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+        else if (moveHorizontal < 0 && moveVertical == 0)               //Going left
+            transform.localEulerAngles = new Vector3(0, 0, 180);
+        else if (moveHorizontal > 0 && moveVertical > 0)                //Going top-right
+            transform.localEulerAngles = new Vector3(0, 0, 45);
+        else if (moveHorizontal < 0 && moveVertical > 0)                //Going top-left
+            transform.localEulerAngles = new Vector3(0, 0, 135);
+        else if (moveHorizontal > 0 && moveVertical < 0)                //Going down-right
+            transform.localEulerAngles = new Vector3(0, 0, 315);
+        else if (moveHorizontal < 0 && moveVertical < 0)                //Going down-left
+            transform.localEulerAngles = new Vector3(0, 0, 230);
     }
 }
